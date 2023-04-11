53db3beab2c1bcfc69913ab6fffc5b9f4cd54a89
dotMaxi <86731788+dotMaxi@users.noreply.github.com>
2023-04-11 01:32:51 +0100

    Create script.js

---
+++ go/crosswind/script.js
@@ -0,0 +1,13 @@
+function calcularComponentes() {
+  const direccion = Number(document.getElementById('direccion').value);
+  const intensidad = Number(document.getElementById('intensidad').value);
+  const pista = Number(document.getElementById('pista').value);
+
+  const direccionPista = pista * 10;
+
+  const vientoCara = Math.round(intensidad * Math.cos((direccion - direccionPista) * Math.PI / 180));
+  const vientoCruzado = Math.round(intensidad * Math.sin((direccion - direccionPista) * Math.PI / 180));
+
+  const resultadosDiv = document.getElementById('resultados');
+  resultadosDiv.innerHTML = `<p>Componente de viento en cara: ${vientoCara} nudos</p><p>Componente de viento cruzado: ${vientoCruzado} nudos</p>`;
+}
