7010735122f2af92ff04bfca96606fb4f5b33600
dotMaxi <86731788+dotMaxi@users.noreply.github.com>
2023-04-11 01:48:37 +0100

    Update script.js

--- go/crosswind/script.js
+++ go/crosswind/script.js
@@ -1,19 +1,40 @@
-function calcularComponentes() {
-  const direccion = Number(document.getElementById('direccion').value);
-  const intensidad = Number(document.getElementById('intensidad').value);
-  const pista = Number(document.getElementById('pista').value);
+// Obtener los elementos del DOM
+const form = document.querySelector('#form');
+const windDirectionInput = document.querySelector('#wind-direction');
+const windSpeedInput = document.querySelector('#wind-speed');
+const runwayInput = document.querySelector('#runway');
+const headwindOutput = document.querySelector('#headwind');
+const crosswindOutput = document.querySelector('#crosswind');
+const runwayImage = document.querySelector('#runway-image');
+const headwindArrow = document.querySelector('#headwind-arrow');
+const crosswindArrow = document.querySelector('#crosswind-arrow');
 
-  const direccionPista = pista * 10;
+// Añadir un evento al formulario para el cálculo al enviar
+form.addEventListener('submit', (e) => {
+  e.preventDefault(); // Evitar que se refresque la página al enviar el formulario
+  const windDirection = parseFloat(windDirectionInput.value);
+  const windSpeed = parseFloat(windSpeedInput.value);
+  const runway = parseFloat(runwayInput.value);
+  const windDirectionRadians = windDirection * (Math.PI / 180);
 
-  const vientoCara = Math.round(intensidad * Math.cos((direccion - direccionPista) * Math.PI / 180));
-  const vientoCruzado = Math.round(intensidad * Math.sin((direccion - direccionPista) * Math.PI / 180));
+  // Cálculo de la componente en cara del viento
+  const headwind = Math.round(windSpeed * Math.cos(windDirectionRadians));
+  headwindOutput.textContent = headwind;
 
-  const resultadosDiv = document.getElementById('resultados');
-  resultadosDiv.innerHTML = `<p>Componente de viento en cara: ${vientoCara} nudos</p><p>Componente de viento cruzado: ${vientoCruzado} nudos</p>`;
-}
+  // Cálculo de la componente cruzada del viento
+  const crosswind = Math.round(windSpeed * Math.sin(windDirectionRadians));
+  crosswindOutput.textContent = crosswind;
 
-document.getElementById('pista').addEventListener('keydown', function(event) {
-  if (event.key === "Enter") {
-    calcularComponentes();
-  }
+  // Actualizar la orientación de las flechas de viento en la imagen de la pista
+  const headwindArrowRotation = windDirection - runway;
+  headwindArrow.style.transform = `rotate(${headwindArrowRotation}deg)`;
+  const crosswindArrowRotation = headwindArrowRotation + 90;
+  crosswindArrow.style.transform = `rotate(${crosswindArrowRotation}deg)`;
+});
+
+// Cargar la imagen de la pista y las flechas de viento después de que se cargue la página
+window.addEventListener('load', () => {
+  runwayImage.src = 'runway.png';
+  headwindArrow.src = 'arrow-blue.png';
+  crosswindArrow.src = 'arrow-red.png';
 });
