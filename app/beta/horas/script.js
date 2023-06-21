80cfc7f0f9abcd3dce3b9d1ca3581287d5912131
dotMaxi <86731788+dotMaxi@users.noreply.github.com>
2023-06-21 16:29:03 +0100

    Create script.js

---
+++ app/beta/horas/script.js
@@ -0,0 +1,25 @@
+function calculateNewTime() {
+  var currentTime = document.getElementById('current-time').value;
+  var addMinutes = parseInt(document.getElementById('add-minutes').value);
+
+  var timeParts = currentTime.split(":");
+  var hours = parseInt(timeParts[0]);
+  var minutes = parseInt(timeParts[1]);
+
+  var totalMinutes = hours * 60 + minutes;
+  var newTotalMinutes = totalMinutes + addMinutes;
+
+  var newHours = Math.floor(newTotalMinutes / 60);
+  var newMinutes = newTotalMinutes % 60;
+
+  if (newHours >= 24) {
+    newHours = newHours % 24;
+  }
+
+  var newTime = formatTime(newHours) + ":" + formatTime(newMinutes);
+  document.getElementById('result').innerHTML = "Nueva hora: " + newTime;
+}
+
+function formatTime(time) {
+  return time.toString().padStart(2, '0');
+}
